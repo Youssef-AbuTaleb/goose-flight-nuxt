ff69b363a0c153de371a4083b525edf0f223a32b
Youssef AbuTaleb <youssefabdelmoneim134@gmail.com>
2024-10-19 04:10:23 +0300

    working on bookin section, add radio buttons component.

---
+++ components/BookingSection/booking-section.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import RadioButton from "./radio-button.vue";
+</script>
+
+<template>
+  <form class="booking">
+    <div class="trip-radio">
+      <RadioButton name="trip-type" id="one-way" label="One Way" />
+      <RadioButton name="trip-type" id="two-way" label="Two Way" />
+    </div>
+
+    <hr class="line" />
+  </form>
+</template>
---
+++ components/BookingSection/radio-button.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="radio-group">
+    <input type="radio" class="radio-input" :id="id" :name="name" />
+    <label :for="id" class="radio-label">
+      <span class="radio-button"></span>
+      {{ label }}
+    </label>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  id: { type: String, required: true },
+  name: { type: String, required: true },
+  label: { type: String, required: true },
+});
+</script>
