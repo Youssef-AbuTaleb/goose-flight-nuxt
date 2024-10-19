e90d4b39b0fbb8c19d0e96488a07d3faf7d8223c
Youssef AbuTaleb <youssefabdelmoneim134@gmail.com>
2024-10-19 20:02:33 +0300

    working on responsive for screens 1400px/1200px/1000px/800px

---
+++ components/Header/burger-menu.vue
@@ -0,0 +1,22 @@
+<template>
+  <button class="burger-menu" @click="toggleMenu">
+    <span class="burger-menu__line"></span>
+    <span class="burger-menu__line"></span>
+    <span class="burger-menu__line"></span>
+  </button>
+  <div class="burger-menu__nav" v-if="showMenu">
+    <a href="#" class="link">{{ $t("header.signIn") }}</a>
+    <a href="#" class="link">{{ $t("header.links.home") }}</a>
+    <a href="#" class="link">{{ $t("header.links.promo") }}</a>
+    <a href="#" class="link">{{ $t("header.links.booking") }}</a>
+    <SwitchLanguageBtn />
+  </div>
+</template>
+
+<script setup lang="ts">
+import SwitchLanguageBtn from "./switch-language-btn.vue";
+const showMenu = ref(false);
+function toggleMenu() {
+  showMenu.value = !showMenu.value;
+}
+</script>
--- components/Header/header.vue
+++ components/Header/header.vue
@@ -2,12 +2,21 @@
 import Logo from "../GeneralComponents/logo.vue";
 import Navbar from "./navbar.vue";
 import SignIn from "./sign-in.vue";
+import SwitchLanguageBtn from "./switch-language-btn.vue";
+import BurgerMenu from "./burger-menu.vue";
 </script>
 
 <template>
   <header class="header">
+    <!--  -->
+    <BurgerMenu />
+
     <Logo />
+
     <Navbar />
-    <SignIn />
+    <div class="header__actions">
+      <SwitchLanguageBtn />
+      <SignIn />
+    </div>
   </header>
 </template>
