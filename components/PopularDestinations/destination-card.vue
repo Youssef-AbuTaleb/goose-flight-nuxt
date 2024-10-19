190210a8ce89d6f4b4f8ce80c84e6d0599facf3c
Youssef AbuTaleb <youssefabdelmoneim134@gmail.com>
2024-10-19 04:09:22 +0300

    working on popular destinations section and destinations cards

---
+++ components/PopularDestinations/destination-card.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="dest-card">
+    <img class="dest-card__img" height="180" :src="imgSrc" :alt="imgAlt" />
+    <p class="dest-card__location">
+      <img src="../../assets/icons/location.svg" alt="location icon" />
+      <span>
+        {{ location }}
+      </span>
+    </p>
+    <p class="dest-card__title">{{ title }}</p>
+    <p class="dest-card__duration" v-if="duration">{{ duration }}</p>
+    <p class="dest-card__price">{{ price }}</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  imgSrc: { type: String, required: true },
+  imgAlt: { type: String, required: true },
+  location: { type: String, required: true },
+  duration: { type: String, required: false },
+  price: { type: String, required: true },
+  title: { type: String, required: true },
+});
+</script>
---
+++ components/PopularDestinations/popular-destinations.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="dest">
+    <h2 class="dest__title">Popular Destinations</h2>
+    <h3 class="dest__subtitle">
+      Vacations to make your experience enjoyable in Indonesia!
+    </h3>
+    <div class="dest__cards">
+      <DestinationCard
+        v-for="dest in destinations"
+        :imgSrc="dest.imgSrc"
+        :imgAlt="dest.alt"
+        :location="dest.location"
+        :duration="dest.duration"
+        :price="dest.price"
+        :title="dest.title"
+      />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import DestinationCard from "./destination-card.vue";
+import destOneImg from "../../assets/imgs/dest-1.png";
+import destTwoImg from "../../assets/imgs/dest-2.png";
+import destThreeImg from "../../assets/imgs/dest-3.png";
+import destFourImg from "../../assets/imgs/dest-4.png";
+
+const destinations = [
+  {
+    imgSrc: destOneImg,
+    location: "Manggarai Barat",
+    title: "Flores Road Trip 3D2N",
+    duration: "3 Days",
+    price: "$375",
+    alt: "destination 1",
+  },
+  {
+    imgSrc: destTwoImg,
+    location: "Bogor",
+    title: "Forrester Glamping Co Bogor",
+    duration: "1 Days",
+    price: "$375",
+    alt: "destination 2",
+  },
+  {
+    imgSrc: destThreeImg,
+    location: "Jakarta",
+    title: "Paket Tiket Pesawat Jakarta Bali",
+    duration: "",
+    price: "$375",
+    alt: "destination 3",
+  },
+  {
+    imgSrc: destFourImg,
+    location: "Kota Semarang",
+    title: "Desa Wisata Kandri",
+    duration: "14 Days",
+    price: "$375",
+    alt: "destination 4",
+  },
+];
+</script>
